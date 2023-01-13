b3366c1e581b63d05a01756d3f580a885cef9cd6
Ridwan Monjur <mjrrdn@gmail.com>
2023-01-13 19:07:26 +0800

    feat(api): added frontpage

--- README.md
+++ README.md
@@ -22,3 +22,10 @@ https://my-json-server.typicode.com/codeLoverX/static-apis
 
 https://my-json-server.typicode.com/codeLoverX/static-apis/posts \
 https://my-json-server.typicode.com/static-apis/profile 
+
+# Vercel app
+
+<h5> Graphql: json-graphql-server/db.js </h5>
+<h5> REST: json-server/db.json </h5>
+
+https://static-apis-jade.vercel.app/db.js
--- server.js
+++ server.js
@@ -2,16 +2,45 @@
 const express = require('express');
 const PORT = 3000;
 const app = express();
-const fs = require('fs');
+const JSON_SERVER_FILE = './json-server/db.json'
+const JSON_GRAPHQL_SERVER_FILE = './json-graphql-server/db.js'
+
+// MAPPINGS
+const json_server_schema = require(JSON_SERVER_FILE)
+const json_graphql_server_schema = require(JSON_GRAPHQL_SERVER_FILE)
+app.get('/', (req, res) => {
+    const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+    res.json({
+        fullUrl,
+        gql: {
+            schema: `${fullUrl}gql`,
+            server: `${fullUrl}grapqhl`
+        },
+        rest: {
+            schema: `${fullUrl}rest`,
+            server: `${fullUrl}api`
+        }
+    })
+});
+app.get('/gql', (req, res) => {
+    res.json({
+        ...json_graphql_server_schema
+    })
+});
+app.get('/rest', (req, res) => {
+    res.json({
+        ...json_server_schema
+    })
+});
 
 // jsonServer
 const jsonServer = require('json-server')
-app.use('/api', jsonServer.router('./json-server/db.json'));
+app.use('/api', jsonServer.router(JSON_SERVER_FILE));
 
 // jsonGraphql
 const jsonGraphqlExpress = require("json-graphql-server").default;
-app.use('/graphql', jsonGraphqlExpress(require('./json-graphql-server/db.js')));
-
+app.use('/graphql', jsonGraphqlExpress(require(JSON_GRAPHQL_SERVER_FILE)));
 app.listen(PORT, () => {
     console.log('JSON Server is running')
 })
